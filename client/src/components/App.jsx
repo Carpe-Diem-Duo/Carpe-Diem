0d4aa0ebd5b96ed1afecdfb6f3a9c2d56a83e722
BrandonRussell2020 <brandon.e.russell4@gmail.com>
2020-12-30 14:23:57 -0800

    Intial Repo Setup

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
+import Styles from '../styles/App.css';
 
-class App extends Component {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  };
+
+  componentDidMount() {
+    //axios
+   };
+
   render() {
     return (
       <div>
